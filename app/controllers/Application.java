ee4654510cacd3dcfac3b80b4cdbd3c41ec53a2c
anunez <angel.nunez@gmail.com>
2015-05-23 20:31:23 -0300

    Expenses update so pending can be saved.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -14,6 +14,7 @@ import views.html.*;
 
 import java.io.*;
 import java.sql.*;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
@@ -50,22 +51,18 @@ public class Application extends Controller {
                     final PreparedStatement stm = connection.prepareStatement(sql);
                     stm.execute();
                 }
-            }
-            catch (Exception e) {
+            } catch (Exception e) {
                 Logger.error("error creating table", e);
                 e.printStackTrace();
             }
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             Logger.error("error getting connection: {}", e.getMessage());
             e.printStackTrace();
-        }
-        finally {
+        } finally {
             if (connection != null) {
                 try {
                     connection.close();
-                }
-                catch (SQLException e) {
+                } catch (SQLException e) {
                     Logger.error("error disconnecting db");
                 }
             }
@@ -75,97 +72,150 @@ public class Application extends Controller {
     }
 
     public static Promise<Result> process() {
-        return new Trace<Promise<Result>>(Application.class, "process")
-                .trace(trace -> {
-                    File shFile = Play.application().getFile("external/process.sh");
-                    File dataFile = Play.application().getFile("data/" + (new SimpleDateFormat("yyyy-MM-dd-HH").format(Calendar.getInstance().getTime())) + "_data.json");
+        return new Trace<Promise<Result>>(Application.class, "process").trace(trace -> {
+            File shFile = Play.application().getFile("external/process.sh");
+            File dataFile = Play.application().getFile(
+                    "data/" + (new SimpleDateFormat("yyyy-MM-dd-HH").format(Calendar.getInstance().getTime())) + "_data.json");
 
-                    try {
-                        String line;
-                        Process p = Runtime.getRuntime().exec(
-                                "sh " + shFile.getAbsolutePath() + " " + dataFile.getAbsolutePath());
-                        BufferedReader bri = new BufferedReader(new InputStreamReader(p.getInputStream()));
-                        BufferedReader bre = new BufferedReader(new InputStreamReader(p.getErrorStream()));
-                        while ((line = bri.readLine()) != null) {
-                            Logger.debug("{}", line);
-                        }
-                        bri.close();
-                        while ((line = bre.readLine()) != null) {
-                            Logger.debug("{}", line);
-                        }
-                        bre.close();
-                        p.waitFor();
-                        System.out.println("Done.");
-                    }
-                    catch (Exception err) {
-                        err.printStackTrace();
-                    }
-
-                    JsonNode node;
-                    try {
-                        node = Json.parse(new FileInputStream(dataFile));
-                    }
-                    catch (FileNotFoundException e) {
-                        Logger.error("cannot open file", e);
-                        return Promise.pure(internalServerError());
-                    }
-
-                    return Promise.promise(() -> {
-                        int count = 0;
-                        Connection connection = null;
-                        try {
-                            connection = DB.getDataSource().getConnection();
-
-                            for (JsonNode item : node) {
-                                if ("pending".equals(item.path("status").asText())) {
-                                    Logger.debug("skipping pending node {}", item);
-                                    continue;
-                                }
-
-                                Logger.debug("inserting node {}", item);
-                                try {
-                                    String sql = "INSERT INTO expenses (date, place, payment, reference, amount, source) VALUES (?,?,?,?,?,?)";
-                                    final PreparedStatement stm = connection.prepareStatement(sql);
-
-                                    long millis = new SimpleDateFormat("yyyy-MM-dd").parse(item.path("date").asText())
-                                            .getTime();
-                                    stm.setDate(1, new Date(millis));
-                                    stm.setString(2, item.path("place").asText());
-                                    stm.setString(3, item.path("payment").asText());
-                                    stm.setString(4, item.path("reference").asText());
-                                    stm.setString(5, item.path("amount").asText());
-                                    stm.setString(6, item.path("source").asText());
-                                    stm.execute();
-
-                                    count++;
-                                }
-                                catch (Exception e) {
-                                    Logger.error("error inserting node: {}, error: {}", item, e.getMessage());
-                                    // e.printStackTrace();
-                        }
-                    }
+            try {
+                String line;
+                Process p = Runtime.getRuntime().exec("sh " + shFile.getAbsolutePath() + " " + dataFile.getAbsolutePath());
+                BufferedReader bri = new BufferedReader(new InputStreamReader(p.getInputStream()));
+                BufferedReader bre = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+                while ((line = bri.readLine()) != null) {
+                    Logger.debug("{}", line);
                 }
-                catch (Exception e) {
+                bri.close();
+                while ((line = bre.readLine()) != null) {
+                    Logger.debug("{}", line);
+                }
+                bre.close();
+                p.waitFor();
+                System.out.println("Done.");
+            } catch (Exception err) {
+                err.printStackTrace();
+            }
+
+            JsonNode node;
+            try {
+                node = Json.parse(new FileInputStream(dataFile));
+            } catch (FileNotFoundException e) {
+                Logger.error("cannot open file", e);
+                return Promise.pure(internalServerError());
+            }
+
+            return Promise.promise(() -> {
+                Connection connection = null;
+                try {
+                    connection = DB.getDataSource().getConnection();
+
+                    for (JsonNode item : node) {
+                        /*if ("pending".equals(item.path("status").asText())) {
+                            Logger.debug("skipping pending node {}", item);
+                            continue;
+                        }*/
+
+                        Long itemId = getItemId(connection, item);
+
+                        if (itemId == null) {
+                            insert(connection, item);
+                        } else {
+                            update(connection, itemId, item);
+                        }
+                    }
+                } catch (Exception e) {
                     Logger.error("error getting connection: {}", e.getMessage());
                     e.printStackTrace();
-                }
-                finally {
+                } finally {
                     if (connection != null) {
                         try {
                             connection.close();
-                        }
-                        catch (SQLException e) {
+                        } catch (SQLException e) {
                             Logger.error("error disconnecting db");
                         }
                     }
                 }
 
-                String s = "done inserted amount: " + String.valueOf(count);
-                Logger.debug(s);
-                return ok(s);
-            })      ;
+                Logger.debug("done");
+                return ok();
+            });
 
-                });
+        });
+    }
+
+    private static Long getItemId(Connection connection, JsonNode item) throws SQLException, ParseException {
+        String sql = "SELECT * FROM expenses WHERE amount = ?";
+        final PreparedStatement stm = connection.prepareStatement(sql);
+
+        stm.setString(1, item.path("amount").asText());
+
+        ResultSet resultSet = stm.executeQuery();
+
+        while (resultSet.next()) {
+            ObjectNode expenseNode = getExpenseNode(resultSet);
+
+            long itemMillis = getMillis(item.path("date").asText());
+            Logger.debug("itemMillis:{}", itemMillis);
+            long dbMillis = expenseNode.path("date").asLong();
+            Logger.debug("dbMillis:{}", dbMillis);
+
+            if(itemMillis != dbMillis) {
+                continue;
+            }
+
+            return expenseNode.get("id").asLong();
+        }
+
+        return null;
+    }
+
+    private static boolean insert(Connection connection, JsonNode item) {
+        Logger.debug("inserting node {}", item);
+        try {
+            String sql = "INSERT INTO expenses (date, place, payment, reference, amount, source) VALUES (?,?,?,?,?,?)";
+            final PreparedStatement stm = connection.prepareStatement(sql);
+
+            long millis = getMillis(item.path("date").asText());
+            stm.setDate(1, new Date(millis));
+            stm.setString(2, item.path("place").asText());
+            stm.setString(3, item.path("payment").asText());
+            stm.setString(4, item.path("reference").asText());
+            stm.setString(5, item.path("amount").asText());
+            stm.setString(6, item.path("source").asText());
+            stm.execute();
+
+            return true;
+        } catch (Exception e) {
+            Logger.error("error inserting node: {}, error: {}", item, e.getMessage());
+            return false;
+        }
+    }
+
+    private static boolean update(Connection connection, long itemId, JsonNode item) {
+        Logger.debug("updating node {}", item);
+        try {
+            String sql = "UPDATE expenses SET date=?, place=?, payment=?, reference=?, amount=?, source=? WHERE id=?";
+            final PreparedStatement stm = connection.prepareStatement(sql);
+
+            long millis = getMillis(item.path("date").asText());
+            stm.setDate(1, new Date(millis));
+            stm.setString(2, item.path("place").asText());
+            stm.setString(3, item.path("payment").asText());
+            stm.setString(4, item.path("reference").asText());
+            stm.setString(5, item.path("amount").asText());
+            stm.setString(6, item.path("source").asText());
+            stm.setLong(7, itemId);
+            stm.execute();
+
+            return true;
+        } catch (Exception e) {
+            Logger.error("error updating node: {}, error: {}", item, e.getMessage());
+            return false;
+        }
+    }
+
+    private static long getMillis(String date) throws ParseException {
+        return new SimpleDateFormat("yyyy-MM-dd").parse(date).getTime();
     }
 
     public static Result expenses() {
@@ -182,35 +232,22 @@ public class Application extends Controller {
                     ResultSet query = stm.executeQuery();
 
                     while (query.next()) {
-
-                        ObjectNode node = JsonNodeFactory.instance.objectNode();
-                        node.put("date", query.getDate("date").getTime());
-                        node.put("place", query.getString("place"));
-                        node.put("payment", query.getString("payment"));
-                        node.put("reference", query.getString("reference"));
-                        node.put("amount", query.getString("amount"));
-                        node.put("source", query.getString("source"));
-
-                        items.add(node);
+                        items.add(getExpenseNode(query));
                     }
 
                     return ok(items);
-                }
-                catch (Exception e) {
+                } catch (Exception e) {
                     Logger.error("error selecting expenses");
                     e.printStackTrace();
                 }
-            }
-            catch (Exception e) {
+            } catch (Exception e) {
                 Logger.error("error getting connection: {}", e.getMessage());
                 e.printStackTrace();
-            }
-            finally {
+            } finally {
                 if (connection != null) {
                     try {
                         connection.close();
-                    }
-                    catch (SQLException e) {
+                    } catch (SQLException e) {
                         Logger.error("error disconnecting db");
                     }
                 }
@@ -218,5 +255,17 @@ public class Application extends Controller {
 
             return ok(items);
         });
+    }
+
+    private static ObjectNode getExpenseNode(ResultSet query) throws SQLException {
+        ObjectNode node = JsonNodeFactory.instance.objectNode();
+        node.put("id", query.getLong("id"));
+        node.put("date", query.getDate("date").getTime());
+        node.put("place", query.getString("place"));
+        node.put("payment", query.getString("payment"));
+        node.put("reference", query.getString("reference"));
+        node.put("amount", query.getString("amount"));
+        node.put("source", query.getString("source"));
+        return node;
     }
 }
